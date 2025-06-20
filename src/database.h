483c5bee4961546a4ebeded0d9439ad4ea0aafac
khaled759 <khalednabil759@gmail.com>
2025-06-21 00:16:39 +0300

    modifiying th .h file

--- src/database.h
+++ src/database.h
@@ -1,46 +1,47 @@
 #ifndef GAME_DATABASE_H
 #define GAME_DATABASE_H
-
+using namespace std;
 #include <string>
 #include <vector>
 #include <utility>
 #include <SQLiteCpp/SQLiteCpp.h>
+#include <string>
 #include "PlayerType.h"
 // Struct to represent a game history
 struct GameHistoryEntry {
     int game_id;
-    std::string opponent_type;
-    std::string opponent;
-    std::string outcome;
-    std::string timestamp;
-    std::vector<Move> game_seq;
+    string opponent_type;
+    string opponent;
+    string outcome;
+    string timestamp;
+    vector<Move> game_seq;
 };
 
 class GameDatabase {
 public:
-    GameDatabase(const std::string& db_path);
+    GameDatabase(const string& db_path);
 
     // Player-related methods
-    bool usernameExists(const std::string& username);
-    void registerPlayer(const std::string& username, const std::string& plain_password);
-    bool verifyPassword(const std::string& username, const std::string& plain_password);
-    std::pair<std::string, std::string> getSaltAndHash(const std::string& username);
+    bool usernameExists(const string& username);
+    void registerPlayer(const string& username, const string& plain_password);
+    bool verifyPassword(const string& username, const string& plain_password);
+    pair<string, string> getSaltAndHash(const string& username);
 
     // Game record methods
-    void recordAIGame(const std::string& player, const std::string& ai_difficulty, const std::string& outcome, const std::vector<Move>& moves);
-    void recordPlayerGame(const std::string& playerA, const std::string& type, const std::string& playerB, const std::string& outcomeA, const std::vector<Move>& moves);
+    void recordAIGame(const string& player, const string& ai_difficulty, const string& outcome, const vector<Move>& moves);
+    void recordPlayerGame(const string& playerA, const string& type, const string& playerB, const string& outcomeA, const vector<Move>& moves);
 
     // History retrieval
-    std::vector<GameHistoryEntry> getGameHistory(const std::string& username);
+    vector<GameHistoryEntry> getGameHistory(const string& username);
 
 private:
     SQLite::Database db;
 
     // Helper methods
-    std::string generateSalt(size_t length = 16);
-    std::string hashPassword(const std::string& password, const std::string& salt_hex);
-    std::string serializeMoves(const std::vector<Move>& moves);
-    std::vector<Move> deserializeMoves(const std::string& moves_str);
+    string generateSalt(size_t length = 16);
+    string hashPassword(const string& password, const string& salt_hex);
+    string serializeMoves(const vector<Move>& moves);
+    vector<Move> deserializeMoves(const string& moves_str);
 };
 
 #endif
